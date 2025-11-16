d50092a4d24a856094902d017ea3169f320089a9
Actlysis <bellomubarak123456@gmail.com>
2025-11-16 06:08:10 +0100

    Delete Privacy-script directory

--- Privacy-script/Privacy_key_extractor.py
+++
@@ -1,3 +0,0 @@
-bytecode = input("Enter the full constructor bytecode for the Privacy level instance:\n").strip()
-length = len(bytecode)
-print(f"bytes16 _key is --->    {bytecode[length - 64:length - 32]}")
